c2adcfdb61e856bd916e3e02fe31d629b173371f
LVCHARVET <lv.charvet28@gmail.com>
2021-11-30 17:07:41 +0100

    Début du panier

--- front/js/product.js
+++ front/js/product.js
@@ -7,29 +7,59 @@ console.log(id);
 
 //on cherche l'API
 
-fetch("http://localhost:3000/api/products/" + id) 
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
+fetch("http://localhost:3000/api/products/" + id)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
 
-        //on pointe
+    //on pointe
 
-        let itemImg = document.querySelector(".item__img")
-        let itemName = document.querySelector("#title")
-        let itemPrice = document.querySelector("#price")
-        let itemDescription = document.querySelector("#description")
-        let itemColors = document.querySelector("#colors")
+    let itemImg = document.querySelector(".item__img");
+    let itemName = document.querySelector("#title");
+    let itemPrice = document.querySelector("#price");
+    let itemDescription = document.querySelector("#description");
+    let itemColors = document.querySelector("#colors");
+    let itemQuantity = document.querySelector("#quantity");
+    let itemAdd = document.querySelector("#addToCart");
 
-        //on injecte
+    //on injecte
 
-        itemImg.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
-        itemName.innerHTML = `${data.name}`
-        itemPrice.innerHTML = `${data.price}`
-        itemDescription.innerHTML = `${data.description}`
+    itemImg.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+    itemName.innerHTML = `${data.name}`;
+    itemPrice.innerHTML = `${data.price}`;
+    itemDescription.innerHTML = `${data.description}`;
 
-        for (let index = 0; index < data.colors.length; index++) {
-            console.log(data.colors[index])
-            itemColors.innerHTML += `<option value="${data.colors[index]}">${data.colors[index]}</option>`            
+    for (let index = 0; index < data.colors.length; index++) {
+      console.log(data.colors[index]);
+      itemColors.innerHTML += `<option value="${data.colors[index]}">${data.colors[index]}</option>`;
+    }
+
+    // On écoute le clique commande
+
+    itemAdd.addEventListener("click", () => {
+
+        // On défini le panier
+
+      let product = {
+        id: data._id,
+        color: itemColors.value,
+        quantity: parseInt(itemQuantity.value),
+      };
+
+      let cart = [];
+
+      // On gére les donnés présente dans le panier
+
+      if (localStorage.getItem("cart") != null) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+        for (prod of cart) {
+          if (prod.id === product.id && prod.color === product.color) {
+            cart.splice(cart.indexOf(prod), 1);
+            product.quantity += prod.quantity;
+          }
         }
-
-    })
+      }
+      cart.push(product);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+  });
